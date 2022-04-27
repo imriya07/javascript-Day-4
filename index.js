e1ce4a0555e7bf3a6df7b69cf4384cabdd80db70
imriya07 <riya01022001@gmail.com>
2022-04-27 10:16:57 +0530

    update index

---
+++ index.js
@@ -0,0 +1,126 @@
+console.log("Working");
+
+//Arithmetic Operators
+//+, -, /, *, %
+
+//modulas - It gives us the remainder
+console.log(2 + 3);
+console.log(2 - 3);
+console.log(2 * 3);
+console.log(2 / 3);
+console.log(2 % 3);
+
+//comparision operators
+//==/!= only check the values and not the data types but ===/!== checks both the values and the datatypes
+
+//< , > , <=, >=, ==, ===
+console.log(2<3);//true(boolean values as answer)
+//console.log(2>3);//false
+//console.log(2>=3);//false
+//console.log(2<=3);//true
+
+console.log(2!=3);//false
+console.log(2!=2);//false
+console.log(2!==2);//false
+console.log(2==2);//true
+console.log(2===2);//true
+console.log(2=="2");//true
+console.log(2==="2");//false
+//Logical operators
+//||, &&, !
+console.log(2||3);//2
+console.log(0||3);//3
+console.log(0||0);//0
+
+console.log(1 && 3);//3
+console.log(0 && 3);//0
+console.log(1 && 0);//0
+
+console.log(!0)//true
+console.log(!1)//false
+console.log(!2)//false
+
+//increment decrement operators
+
+let a = 1;
+a++;//a = a+1
+console.log(a);//2
+a--;//a = a-1
+console.log(a);//1
+//a = 1
+console.log(a++);//1//i will first print the value of a, and then increment its value with 1 which will be reflected/shown in next line,not on the same line
+console.log(a);//2
+
+console.log(a--);//1//i will first print the value of a, and then increment its value with 1 which will be reflected/shown in next line,not on the same line
+console.log(a);//0
+let b = 2;
+a*=3; // b=b*3
+b/=3; // b = b/3;
+b+=2;// b = b+2;
+b-=4;//b = b-4;
+
+let c = 3;
+console.log(c);//3
+console.log(++c)//4//first increment /decrement the value based on the operator
+
+
+//if
+//else
+//switch
+
+//if blocks work
+
+//function nameofFn(){
+
+//}
+
+
+
+//how does if loop work?
+//if a certain condition written inside the parantesis of if block is true then we enter inside the body of if/ basically enters inside the curly 
+
+
+let g = 3;
+if(g>4){
+    console.log(g);
+}
+else{
+    console.log(enter);
+}
+
+if(g>4){
+    console.log(g);
+}
+else if(g==3){
+    console.log("g value is 3")
+}
+else{
+    console.log("Enterd innside the else block");
+}
+
+let number = 6;
+switch (number){
+    case 1:
+        console.log("I am one");
+        break;
+     case 2:
+        console.log("I am two");
+        break;
+    case 3:
+        console.log("I am three");
+        break; 
+    case 4:
+        console.log("I am four");
+        break; 
+    default:
+        console.log("I will be greater than four ");
+        break;  
+}
+
+
+
+
+
+
+
+
